0985a5c87d17ab0928bf78caa937a727515099b5
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-11-02 12:37:19 +0330

    Added sticky header & sidenav menu

--- public/assets/js/main-scripts.js
+++ public/assets/js/main-scripts.js
@@ -5,9 +5,33 @@
         // Launch Functions
         Launch: function () {
             fn.Test();
+            fn.Sidenav();
+            fn.StickyHeader();
         },
         Test: function () {
             console.log("Worked!");
+        },
+        Sidenav: function () {
+            var button = $(".button-display-sidenav");
+            var sidenav = $(".app-sidenav");
+
+            button.on("click", function (e) {
+                e.preventDefault();
+                sidenav.addClass("active");
+            });
+            sidenav.on("click", ".button-close-sidenav, .sidenav-close", function () {
+                sidenav.removeClass("active");
+            });
+        },
+        StickyHeader: function () {
+            var header = $(".app-header");
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > header.height()) {
+                    header.addClass("sticky");
+                } else {
+                    header.removeClass("sticky");
+                }
+            });
         },
     };
 
