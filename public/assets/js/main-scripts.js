38338c0fe57b66f967ae8f4e5a5cdbc87c3e7532
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-11-03 17:23:04 +0330

    Added portfolio style 15

--- public/assets/js/main-scripts.js
+++ public/assets/js/main-scripts.js
@@ -6,6 +6,7 @@
         Launch: function () {
             fn.Test();
             fn.Sidenav();
+            fn.Carousel();
         },
         Test: function () {
             console.log("Worked!");
@@ -20,6 +21,37 @@
             });
             sidenav.on("click", ".button-close-sidenav, .sidenav-close", function () {
                 sidenav.removeClass("active");
+            });
+        },
+        Carousel: function () {
+            var mySwiper = new Swiper(".el-swiper-portfolio-15", {
+                loop: false,
+                navigation: {
+                    nextEl: ".el-slider-next",
+                    prevEl: ".el-slider-prev",
+                },
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1.2,
+                        spaceBetween: 12,
+                    },
+                    576: {
+                        slidesPerView: 2,
+                        spaceBetween: 12,
+                    },
+                    768: {
+                        slidesPerView: 2.3,
+                        spaceBetween: 18,
+                    },
+                    992: {
+                        slidesPerView: 3.3,
+                        spaceBetween: 18,
+                    },
+                    1200: {
+                        slidesPerView: 4,
+                        spaceBetween: 18,
+                    },
+                },
             });
         },
     };
